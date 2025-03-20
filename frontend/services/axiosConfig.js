fd32034088d62316f4a2be251310bce46a100d3f
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2025-03-20 14:13:10 -0300

    Update axiosConfig.js

--- frontend/services/axiosConfig.js
+++ frontend/services/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const DEBUG = false;
 
 const axiosInstance = axios.create({
-  baseURL: DEBUG ? 'http://localhost:8000/api/v1/' : 'http://10.0.129.164:8001/api/v1/',
+  baseURL: DEBUG ? 'http://localhost:8000/api/v1/' : 'https://10.0.129.164/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
